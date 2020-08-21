10071369a1b354fdb6bec1384e35ad33d07b5df2
ankith88 <ankith88@gmail.com>
2020-08-22 09:32:32 +1000

    New - Weekly Usage

---
+++ mp_ss_update_mpex_usage_per_week.js
@@ -0,0 +1,70 @@
+/**
+ * Module Description
+ * 
+ * NSVersion    Date            			Author         
+ * 1.00       	2020-08-21 11:31:55   		Ankith
+ *
+ * Description:  Update the Weekly MPEX Usage on the Customer Record.       
+ * 
+ * @Last Modified by:   Ankith
+ * @Last Modified time: 2020-08-22 09:32:29
+ *
+ */
+var adhoc_inv_deploy = 'customdeploy2';
+var prev_inv_deploy = null;
+var ctx = nlapiGetContext();
+
+function mpexUsagePerWeek() {
+
+    prev_inv_deploy = ctx.getDeploymentId();
+
+    /**
+     * MPEX Usage - Per Week
+     */
+    var mpexUsagePerWeek = nlapiLoadSearch('customrecord_customer_product_stock', 'customsearch_mpex_usage_per_week_2');
+    var resultMPEXUsagePerWeek = mpexUsagePerWeek.runSearch();
+
+    var oldCustomerInternalID = null;
+    var count = 0;
+    var data = '{[';
+
+    resultMPEXUsagePerWeek.forEachResult(function(searchResult) {
+
+        var customerInternalID = searchResult.getValue("internalid", "CUSTRECORD_CUST_PROD_STOCK_CUSTOMER", "GROUP");
+        var dateStockUsedWeek = searchResult.getValue("custrecord_cust_date_stock_used", null, "GROUP");
+        var usageCount = searchResult.getValue("name", null, "COUNT");
+
+        if (oldCustomerInternalID != null && oldCustomerInternalID != customerInternalID) {
+            data = data.substring(0, data.length - 1);
+            data += ']}';
+            nlapiLogExecution('AUDIT', 'data', data);
+
+            var customerRecord = nlapiLoadRecord('customer', oldCustomerInternalID);
+            customerRecord.setFieldValue('custentity_actual_mpex_weekly_usage', data);
+            customerRecord.setFieldValue('custentity_mpex_weekly_usage_calculated', 1);
+            nlapiSubmitRecord(customerRecord);
+
+            reschedule = rescheduleScript(prev_inv_deploy, adhoc_inv_deploy, null);
+            nlapiLogExecution('AUDIT', 'Reschedule Return', reschedule);
+            if (reschedule == false) {
+                return false;
+            }
+        } else {
+            data += '"Week Used" : "' + dateStockUsedWeek + '",';
+            data += '"Count" : "' + usageCount + '",';
+        }
+
+
+        oldCustomerInternalID = customerInternalID;
+        count++;
+        return true;
+    });
+
+    if (count > 0) {
+        data = data.substring(0, data.length - 1);
+        data += ']}';
+    } else {
+        data += ']}';
+    }
+
+}
