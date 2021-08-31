e6828d5f122d9373e23f4f9003b7d09100a70e5c
ankith88 <ankith88@gmail.com>
2021-08-31 14:58:15 +1000

    update

--- Customer Product Stock - Update Record/mp_ss_mpex_update_record_V2.js
+++ Customer Product Stock - Update Record/mp_ss_mpex_update_record_V2.js
@@ -5,8 +5,8 @@
  * 
  * Description: Update the new fields based on the product name
  * 
- * @Last Modified by: Ankith
- * @Last Modified time: 2020-08-05 11:41:56
+ * @Last Modified by:   ankit
+ * @Last Modified time: 2021-08-30 14:46:33
  * 
  */
 
@@ -17,16 +17,18 @@ var ctx = nlapiGetContext();
 function main() {
 
 	nlapiLogExecution('AUDIT', 'prev_deployment', ctx.getSetting('SCRIPT',
-			'custscript_prev_deploy_create_prod_order'));
+		'custscript_prev_deploy_create_prod_order'));
 
 	prev_inv_deploy = ctx.getDeploymentId();
 
 	/**
 	 * MPEX - Update Record Fields
+	 * New Search: MPEX - Update Record Fields (New Pricing)
+	 * Update Search ID to: customsearch_mpex_update_record_fields_3
 	 */
 	var createProdOrderSearch = nlapiLoadSearch(
-			'customrecord_customer_product_stock',
-			'customsearch_mpex_update_record_fields');
+		'customrecord_customer_product_stock',
+		'customsearch_mpex_update_record_fields');
 	var resultCreateProdOrder = createProdOrderSearch.runSearch();
 
 	var old_customer_id = null;
@@ -37,132 +39,135 @@ function main() {
 	 * Go through each line item from the search.
 	 */
 	resultCreateProdOrder
-			.forEachResult(function(searchResult) {
-				var internalid = searchResult.getValue("internalid");
-				var prod_name = searchResult
-						.getValue("custrecord_cust_stock_prod_name");
+		.forEachResult(function(searchResult) {
+			var internalid = searchResult.getValue("internalid");
+			var prod_name = searchResult
+				.getValue("custrecord_cust_stock_prod_name");
 
-				var GOLD_TOLL = searchResult.getValue(
-						"custrecord_ap_item_single_name_toll",
-						"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
-				var GOLD_MP = searchResult.getValue(
-						"custrecord_ap_item_single_name_mp",
-						"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
-				var SP_MP = searchResult.getValue(
-						"custrecord_ap_item_standard_mp_rate",
-						"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
-				var SP_TOLL = searchResult.getValue(
-						"custrecord_ap_item_standard_toll_rate",
-						"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
-				var PP_MP = searchResult.getValue(
-						"custrecord_ap_item_single_name_3rd_mp",
-						"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
-				var PP_TOLL = searchResult.getValue(
-						"custrecord_ap_item_single_name_3rd_toll",
-						"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
-				var DIRECT_TOLL = searchResult.getValue(
-						"custrecord_ap_item_direct_toll_rate",
-						"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
-				var DIRECT_MP = searchResult.getValue(
-						"custrecord_ap_item_direct_mp",
-						"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
-				var PRO_MP = searchResult.getValue(
-						"custrecord_ap_item_pro_name_mp",
-						"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
-				var PRO_TOLL = searchResult.getValue(
-						"custrecord_ap_item_pro_name_toll",
-						"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
-				var PRO_GOLD_MP = searchResult.getValue(
-						"custrecord_ap_item_pro_gold_mp",
-						"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
-				var PRO_GOLD_TOLL = searchResult.getValue(
-						"custrecord_ap_item_pro_gold_toll",
-						"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
+			//OLD PRICING
+			var OLD_GOLD_TOLL = searchResult.getValue(
+				"custrecord_ap_item_single_name_toll",
+				"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
+			var OLD_GOLD_MP = searchResult.getValue(
+				"custrecord_ap_item_single_name_mp",
+				"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
+			var OLD_SP_MP = searchResult.getValue(
+				"custrecord_ap_item_standard_mp_rate",
+				"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
+			var OLD_SP_TOLL = searchResult.getValue(
+				"custrecord_ap_item_standard_toll_rate",
+				"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
+			var OLD_PP_MP = searchResult.getValue(
+				"custrecord_ap_item_single_name_3rd_mp",
+				"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
+			var OLD_PP_TOLL = searchResult.getValue(
+				"custrecord_ap_item_single_name_3rd_toll",
+				"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
+			var OLD_DIRECT_TOLL = searchResult.getValue(
+				"custrecord_ap_item_direct_toll_rate",
+				"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
+			var OLD_DIRECT_MP = searchResult.getValue(
+				"custrecord_ap_item_direct_mp",
+				"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
+			var OLD_PRO_MP = searchResult.getValue(
+				"custrecord_ap_item_pro_name_mp",
+				"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
+			var OLD_PRO_TOLL = searchResult.getValue(
+				"custrecord_ap_item_pro_name_toll",
+				"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
+			var PRO_OLD_GOLD_MP = searchResult.getValue(
+				"custrecord_cust_prod_stock_pro_gold_mp",
+				"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
+			var PRO_OLD_GOLD_TOLL = searchResult.getValue(
+				"custrecord_cust_prod_stock_pro_gold_toll",
+				"CUSTRECORD_CUST_STOCK_PROD_NAME", null);
 
-				 nlapiLogExecution('DEBUG', 'internalid', internalid);
-				// nlapiLogExecution('DEBUG', 'internalid', pro_MP);
-				// nlapiLogExecution('DEBUG', 'internalid', pro_toll);
+			//NEW PRICING
 
-				var mpexRecord = nlapiLoadRecord(
-						'customrecord_customer_product_stock', internalid);
+			nlapiLogExecution('DEBUG', 'internalid', internalid);
+			// nlapiLogExecution('DEBUG', 'internalid', pro_MP);
+			// nlapiLogExecution('DEBUG', 'internalid', pro_toll);
 
-				// UPDATE STANDARD PRICE POINTS IF EMPTY
-				if (isNullorEmpty(mpexRecord
-						.getFieldValue('custrecord_mpex_standard_mp_rate'))) {
-					mpexRecord.setFieldValue(
-							'custrecord_mpex_standard_mp_rate', SP_MP);
-				}
-				if (isNullorEmpty(mpexRecord
-						.getFieldValue('custrecord_mpex_standard_toll_rate'))) {
-					mpexRecord.setFieldValue(
-							'custrecord_mpex_standard_toll_rate', SP_TOLL);
-				}
+			var mpexRecord = nlapiLoadRecord(
+				'customrecord_customer_product_stock', internalid);
 
-				// UPDATE GOLD PRICE POINTS IF EMPTY
-				if (isNullorEmpty(mpexRecord
-						.getFieldValue('custrecord_cust_prod_stock_single_name'))) {
-					mpexRecord
-							.setFieldValue(
-									'custrecord_cust_prod_stock_single_name',
-									GOLD_TOLL);
-				}
-				if (isNullorEmpty(mpexRecord
-						.getFieldValue('custrecord_cust_prod_stock_name_mp'))) {
-					mpexRecord.setFieldValue(
-							'custrecord_cust_prod_stock_name_mp', GOLD_MP);
-				}
+			// UPDATE STANDARD PRICE POINTS IF EMPTY
+			if (isNullorEmpty(mpexRecord
+					.getFieldValue('custrecord_mpex_standard_mp_rate'))) {
+				mpexRecord.setFieldValue(
+					'custrecord_mpex_standard_mp_rate', OLD_SP_MP);
+			}
+			if (isNullorEmpty(mpexRecord
+					.getFieldValue('custrecord_mpex_standard_toll_rate'))) {
+				mpexRecord.setFieldValue(
+					'custrecord_mpex_standard_toll_rate', OLD_SP_TOLL);
+			}
 
-				// UPDATE PLATINUM PRICE POINTS IF EMPTY
-				if (isNullorEmpty(mpexRecord
-						.getFieldValue('custrecord_cust_prod_stock_3rd_party_tol'))) {
-					mpexRecord
-							.setFieldValue(
-									'custrecord_cust_prod_stock_3rd_party_tol',
-									PP_TOLL);
-				}
-				if (isNullorEmpty(mpexRecord
-						.getFieldValue('custrecord_cust_prod_stock_3rd_party_mp'))) {
-					mpexRecord.setFieldValue(
-							'custrecord_cust_prod_stock_3rd_party_mp', PP_MP);
-				}
+			// UPDATE GOLD PRICE POINTS IF EMPTY
+			if (isNullorEmpty(mpexRecord
+					.getFieldValue('custrecord_cust_prod_stock_single_name'))) {
+				mpexRecord
+					.setFieldValue(
+						'custrecord_cust_prod_stock_single_name',
+						OLD_GOLD_TOLL);
+			}
+			if (isNullorEmpty(mpexRecord
+					.getFieldValue('custrecord_cust_prod_stock_name_mp'))) {
+				mpexRecord.setFieldValue(
+					'custrecord_cust_prod_stock_name_mp', OLD_GOLD_MP);
+			}
 
-				// UPDATE PRO PLATINUM PRICE POINTS IF EMPTY
-				if (isNullorEmpty(mpexRecord
-						.getFieldValue('custrecord_cust_prod_stock_pro_toll'))) {
-					mpexRecord.setFieldValue(
-							'custrecord_cust_prod_stock_pro_toll', PRO_TOLL);
-				}
-				if (isNullorEmpty(mpexRecord
-						.getFieldValue('custrecord_cust_prod_stock_pro_mp'))) {
-					mpexRecord.setFieldValue(
-							'custrecord_cust_prod_stock_pro_mp', PRO_MP);
-				}
+			// UPDATE PLATINUM PRICE POINTS IF EMPTY
+			if (isNullorEmpty(mpexRecord
+					.getFieldValue('custrecord_cust_prod_stock_3rd_party_tol'))) {
+				mpexRecord
+					.setFieldValue(
+						'custrecord_cust_prod_stock_3rd_party_tol',
+						OLD_PP_TOLL);
+			}
+			if (isNullorEmpty(mpexRecord
+					.getFieldValue('custrecord_cust_prod_stock_3rd_party_mp'))) {
+				mpexRecord.setFieldValue(
+					'custrecord_cust_prod_stock_3rd_party_mp', OLD_PP_MP);
+			}
 
-				// UPDATE PRO GOLD PRICE POINTS IF EMPTY
-				if (isNullorEmpty(mpexRecord
-						.getFieldValue('custrecord_cust_prod_stock_pro_gold_toll'))) {
-					mpexRecord.setFieldValue(
-							'custrecord_cust_prod_stock_pro_gold_toll',
-							PRO_GOLD_TOLL);
-				}
-				if (isNullorEmpty(mpexRecord
-						.getFieldValue('custrecord_cust_prod_stock_pro_gold_mp'))) {
-					mpexRecord.setFieldValue(
-							'custrecord_cust_prod_stock_pro_gold_mp',
-							PRO_GOLD_MP);
-				}
+			// UPDATE PRO PLATINUM PRICE POINTS IF EMPTY
+			if (isNullorEmpty(mpexRecord
+					.getFieldValue('custrecord_cust_prod_stock_pro_toll'))) {
+				mpexRecord.setFieldValue(
+					'custrecord_cust_prod_stock_pro_toll', OLD_PRO_TOLL);
+			}
+			if (isNullorEmpty(mpexRecord
+					.getFieldValue('custrecord_cust_prod_stock_pro_mp'))) {
+				mpexRecord.setFieldValue(
+					'custrecord_cust_prod_stock_pro_mp', OLD_PRO_MP);
+			}
 
-				nlapiSubmitRecord(mpexRecord);
+			// UPDATE PRO GOLD PRICE POINTS IF EMPTY
+			if (isNullorEmpty(mpexRecord
+					.getFieldValue('custrecord_cust_prod_stock_pro_gold_toll'))) {
+				mpexRecord.setFieldValue(
+					'custrecord_cust_prod_stock_pro_gold_toll',
+					PRO_OLD_GOLD_TOLL);
+			}
+			if (isNullorEmpty(mpexRecord
+					.getFieldValue('custrecord_cust_prod_stock_pro_gold_mp'))) {
+				mpexRecord.setFieldValue(
+					'custrecord_cust_prod_stock_pro_gold_mp',
+					PRO_OLD_GOLD_MP);
+			}
 
-				reschedule = rescheduleScript(prev_inv_deploy,
-						adhoc_inv_deploy, null);
-				nlapiLogExecution('AUDIT', 'Reschedule Return', reschedule);
-				if (reschedule == false) {
+			nlapiSubmitRecord(mpexRecord);
 
-					return false;
-				}
+			reschedule = rescheduleScript(prev_inv_deploy,
+				adhoc_inv_deploy, null);
+			nlapiLogExecution('AUDIT', 'Reschedule Return', reschedule);
+			if (reschedule == false) {
 
-				return true;
+				return false;
+			}
 
-			});
+			return true;
+
+		});
 }
--- MPEX Invoicing/mp_ss_mpex_weekly_prod_inv_list_v2.js
+++ MPEX Invoicing/mp_ss_mpex_weekly_prod_inv_list_v2.js
@@ -6,8 +6,8 @@
  *
  * Description: Create MPEX Invoices at the end of every week            
  * 
- * @Last Modified by:   Ankith
- * @Last Modified time: 2020-06-16 14:36:03
+ * @Last Modified by:   ankit
+ * @Last Modified time: 2021-08-31 12:30:50
  *
  */
 
@@ -196,8 +196,8 @@ function main(type) {
                         recInvoice.setFieldValue('entity', searchResults[n].getValue('custrecord_ap_order_customer', null, "GROUP"));
                         recInvoice.setFieldValue('department', nlapiLoadRecord('partner', 435).getFieldValue('department'));
                         recInvoice.setFieldValue('location', nlapiLoadRecord('partner', 435).getFieldValue('location'));
-//                        recInvoice.setFieldValue('trandate', tranDate);
-                        recInvoice.setFieldValue('trandate', '22/08/2021');
+                       recInvoice.setFieldValue('trandate', tranDate);
+                        // recInvoice.setFieldValue('trandate', '22/08/2021');
                         recInvoice.setFieldValue('custbody_inv_date_range_from', searchResults[n].getValue('custrecord_ap_order_fulfillment_date', null, "GROUP"));
                         recInvoice.setFieldValue('custbody_inv_date_range_to', searchResults[n].getValue('custrecord_ap_order_fulfillment_date', null, "GROUP"));
                         // recInvoice.setFieldValues('custbody_ap_product_order', internal_id);
